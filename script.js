a9fd10a7f2b5f1ccc7d43d48b2ca5a41b41c71e8
stin029j@stud.kea.dk <>
2019-01-06 15:14:12 +0100

    content_ready

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+window.addEventListener("load", sidenVises);
 
+//"use strict";
+//var timeLeft;
+
+
+function sidenVises() {
+    console.log("siden vises");
+    document.querySelector(".banner_inside").classList.add("fade_in");
+    document.querySelector(".arrow").classList.add("pulse");
+}
+
+//function clickArrow {
+//    console.log("clickArrow");
+//    document.querySelector(".arrow").addEventListener("click");
+//}
