a469eff0b521d10d2ba9fd06e66b2b1fd3ab25c5
stin029j@stud.kea.dk <>
2019-01-08 10:49:08 +0100

    content_almostdone

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
-window.addEventListener("load", sidenVises);
+window.addEventListener("load", function () {
+    const loader = document.querySelector(".loader");
+    console.log("loader");
+    loader.className += " fade_out"
+    sidenVises();
+    fadeInBanner();
+});
 
-//"use strict";
-//var timeLeft;
 
 
 function sidenVises() {
     console.log("siden vises");
+
+}
+
+
+
+document.getElementById('x').addEventListener('click', function () {
+    if (this.classList.contains('clicked')) {
+        this.classList.remove('clicked');
+        document.querySelector(".sidebar").classList.add("hide");
+    } else {
+        this.classList.add('clicked');
+        document.querySelector(".sidebar").classList.remove("hide");
+    }
+
+});
+
+
+function fadeInBanner() {
     document.querySelector(".banner_inside").classList.add("fade_in");
     document.querySelector(".arrow").classList.add("pulse");
 }
-
-//function clickArrow {
-//    console.log("clickArrow");
-//    document.querySelector(".arrow").addEventListener("click");
-//}
